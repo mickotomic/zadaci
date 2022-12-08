2b21496e94c16d71067fbf6b3e39aaa84ed2bafe
mickotomic <milorad.tomic.87@gmail.com>
2022-12-08 20:09:27 +0100

    Update zadatakTajmer.js

---
+++ zadatakTajmer.js
@@ -0,0 +1,20 @@
+//setTimeout(() => {console.log("Kasnimo  5 sekundi.");}, 5000);
+
+// console.log("First");
+// setTimeout(() => {console.log("second");}, 0);
+// console.log("Third");
+
+// setTimeout(function() {
+
+// }, 55555)
+let i = 0;
+setTimeout(() => {console.log("second");}, 0);
+
+while (i < 1000000000) {
+
+    i++ ;
+
+}
+console.log("vreme");
+setTimeout(() => {console.log("time");}, 0);
+
