badb99e3baf6aee94f6540ae11fdaae78c97cf2c
mickotomic <milorad.tomic.87@gmail.com>
2022-12-13 17:19:12 +0100

    Update callbacks.js and promises1.js

--- promises1.js
+++ promises1.js
@@ -1,36 +1,36 @@
-let cleanRoom = function(){
-    return new Promise(function(resolve, reject){
-        resolve ("Room is cleaned!");
-    });
+let cleanRoom = function () {
+  return new Promise(function (resolve, reject) {
+    resolve("Room is cleaned!");
+  });
 };
 
-let removeGarbage = function(message){
-    return new Promise(function(resolve, reject){
-        resolve (message + "Garbage is removed!");
-    });
+let removeGarbage = function (message) {
+  return new Promise(function (resolve, reject) {
+    resolve(message + "Garbage is removed!");
+  });
 };
 
-let winIceCream = function(message){
-    return new Promise (function(resolve, reject){
-        resolve (message + "We won icecream!");
-    });
+let winIceCream = function (message) {
+  return new Promise(function (resolve, reject) {
+    resolve(message + "We won icecream!");
+  });
 };
 
-cleanRoom().then(function(result){
+cleanRoom()
+  .then(function (result) {
     return removeGarbage(result);
-}).then (function (result){
+  })
+  .then(function (result) {
     return winIceCream(result);
-}).then (function(result){
+  })
+  .then(function (result) {
     console.log("The job is done!" + result);
+  });
+
+Promise.all([cleanRoom(), removeGarbage(), winIceCream()]).then(function () {
+  console.log("All are finished!");
 });
 
-
-Promise.all([cleanRoom(), removeGarbage(), winIceCream()])
-.then(function(){
-    console.log("All are finished!");
-});
-
-Promise.race([cleanRoom(), removeGarbage(), winIceCream()])
-.then(function(){
-    console.log("One has finished first!");
+Promise.race([cleanRoom(), removeGarbage(), winIceCream()]).then(function () {
+  console.log("One has finished first!");
 });
