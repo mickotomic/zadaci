3c17dad92258a7d74eb09ae32e31700086a90d44
mickotomic <milorad.tomic.87@gmail.com>
2022-12-13 19:37:16 +0100

    Update promises1.js

--- promises1.js
+++ promises1.js
@@ -34,3 +34,25 @@ Promise.all([cleanRoom(), removeGarbage(), winIceCream()]).then(function () {
 Promise.race([cleanRoom(), removeGarbage(), winIceCream()]).then(function () {
   console.log("One has finished first!");
 });
+
+
+const promise = new Promise(function(resolveFunction, rejectFunction) {
+    // Ovdje ide kod koji ce potrajati neko vrijeme
+    resolveFunction("OK"); // ova funkcija se izvrsava ako je sve ok
+    rejectFunction("Error");  // ova funkcija se izvrsava ako dodje do problema
+});
+
+    // ovo je kod koji konzumira promise i mora cekati na rezultat
+promise.then(
+    function(value) {
+        console.log(value)
+        // ovaj kod ce se izvrsiti ako je promise uspio 
+    },
+    function(error) {
+        console.log(error)
+        // ovaj kod ce se izvrsiti u slucaju greske 
+    }
+);
+
+console.log("I'm still the number one!");
+
